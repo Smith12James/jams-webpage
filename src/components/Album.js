6956699439069ce87a4479e133463c66bd4e9907
James Smith <james@Jamess-MacBook-Air.local>
2019-01-28 20:47:49 -0700

    songs not defined

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Album extends Component {
@@ -8,7 +7,7 @@ class Album extends Component {
 
 
     const album = albumData.find( album => {
-      return album.slug === this.props.match.params.slug
+      return album.slug === this.props.match.params.slug;
     });
 
     this.state = {
@@ -17,45 +16,46 @@ class Album extends Component {
       isPlaying: false
     };
 
+
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
 
   }
 
-play() {
-  this.audioElement.play();
-  this.setState({ isPlaying: true });
-}
-
-pause() {
-  this.audioElement.pause();
-  this.setState({ isPlaying: false });
-}
-
-setSong(song) {
-  this.audioElement.src = song.audioSrc;
-  this.setState({ currentSong: song });
-}
-
-handleSongClick(song) {
-  const isSameSong = this.state.currentSong === song;
-  if (this.state.isPlaying && isSameSong) {
-    this.pause();
-  } else {
-    if (!isSameSong) { this.setSong(song); }
-    this.play();
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
   }
-}
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
 
   render() {
     return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src ={this.state.album.albumCover} alt={this.state.album.title}/>
+          <img id="album-cover-art" src={this.state.album.albumCover} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
+            <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
           </div>
         </section>
         <table id="song-list">
@@ -65,11 +65,17 @@ handleSongClick(song) {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
-            {this.state.album.songs.map((song,index)=>
-              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
-                <td id="song-number">{song.index}</td>
-                <td id="song-title">{song.title}</td>
-                <td id="song-duration">{song.duration}</td>
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={ () => this.handleSongClick(song)} >
+                <td className="song-actions">
+                  <button>
+                    <span className="song-number">{index+1}</span>
+                    <span className="ion-play"></span>
+                    <span className="ion-pause"></span>
+                  </button>
+                </td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
               </tr>
             )}
           </tbody>
