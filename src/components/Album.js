3f99be36cc40e96cbf62e4e0459342c8be7fc0b5
James Smith <james@Jamess-MacBook-Air.local>
2019-01-29 20:24:15 -0700

    updated audio Playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -65,12 +65,15 @@ class Album extends Component {
           </colgroup>
           <tbody>
             {this.state.album.songs.map( (song, index) =>
-              <tr className="song" key={index} onClick={ () => this.handleSongClick(song)} >
+              <tr className="song" key={index} onClick={ () => this.handleSongClick(song)}
+              onMouseEnter={() => this.setState({ isHovered: index + 1 })}
+              onMouseLeave={() => this.setState({ isHovered: false })}>
                 <td className="song-actions">
-                  <button>
-                    <span className="song-number">{index+1}</span>
-                    <span className="ion-play"></span>
-                    <span className="ion-pause"></span>
+                  <button id="play-pause">
+                    {this.state.currentSong.title === song.title ?
+                    (<span className={this.state.isPlaying ? "ion-pause" : "ion-play"} />) :
+                    this.state.isHovered === index +1 ? (<span className="ion-play" />) :
+                    (<span className="song-number">{index + 1}</span>)}
                   </button>
                 </td>
                 <td className="song-title">{song.title}</td>
